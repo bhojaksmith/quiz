3e6c4470f4afbc4370dd3deccc1c4aba0832b524
Smith Bhojak <99sbhojak@gmail.com>
2016-03-20 07:05:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+confirm("Click OK if You are Ready")
+  var name = prompt("Enter Your name ");
+  var age = prompt("Enter Your age:-");
+if (age < 16){
+    alert("Play at Your Own Risk");
+    }
+    else {
+        confirm("Game on");
+        }
+        confirm("Answer the Following in t or f ");
+        
+      var uA = prompt("1)Dennis Ritchie Invented C");
+      if (uA === "t"){
+        confirm("True is Correct Answer");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong correct answer is True");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+          
+          
+      var uA = prompt("2)() punctuation is used to signal the beginning and end of code blocks?");
+      if (uA === "f"){
+        confirm("You are right  correct answer is {} ");
+      }
+      else if (uA === "f"){
+        confirm("you are wrong correct answer is {}");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("3); punctuation ends most lines of C code");
+      if (uA === "t"){
+        confirm("You are Correct ; ends most lines of c code ");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong ; ends most lines of c code");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("4)== is the correct operator to compare two variables?");
+      if (uA === "t"){
+        confirm("you are Correct  == is Correct operator to compare ");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong == is Correct operator to compare");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("5)start() is the only function all C programs must contain?");
+      if (uA === "f"){
+        confirm("false is correct main() is right answer");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong , main() is right answer");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("6)there is no differnce between While Loop & For Looop");
+      if (uA === "f"){
+        confirm("you are Correct  there is a differnce between them");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong there is a differnce between them");
+    }
+    else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("7)Is void a variable?");
+      if (uA === "f"){
+        confirm("you are Wrong void is a variable ");
+      }
+      else if (uA === "f"){
+          confirm("You are right void is variable");
+      }
+      else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("8)Can keywords be used as variables?");
+      if (uA === "f"){
+        confirm("you are Correct keywords cannot be used as variables");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong");
+    }
+    else { confirm("please Enter in 't' & 'f' ");}
+      var uA = prompt("9)Can we use Symbols in algorithm?");
+      if (uA === "f"){
+        confirm("You are correct");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong we cannot use symbolls i algorithm");
+    }
+    else { confirm("please Enter in 't' & 'f' ");}
+     var uA = prompt("10)Console log inout output is a header file ?");
+      if (uA === "t"){
+        confirm("you are Correct ");
+      }
+      else if (uA === "f"){
+        confirm("You are wrong ");
+    }
+    else { confirm("please Enter in 't' & 'f' ");}
+        var feedback= prompt("Rate This Game From 1 to 10");
+        if (feedback > 6)
+        { confirm("Thank you   " + name  + " !");
+            }
+            else {
+                confirm("I'll keep practicing coding " + name + "!");
+                }
+                
